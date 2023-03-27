1b19c1b611855df2e5fa337c85af0d23e67cb4ef
marquinho94 <marcoalanmiranda@gmail.com>
2023-03-27 14:57:31 -0300

    cree la funcion para leer el pulsador 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
 
+
+
+// hola, este es un comentario para mostrar como funciona git 
+void funcionDePrueba ();
+int lecturaPulsador1(){
+
+  return digitalRead(1);
+}
 void setup() {
+
   // put your setup code here, to run once:
 }
 
